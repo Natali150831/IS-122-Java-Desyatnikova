cc73adb04f87f9e336e5f819252913fdf7633231
natali <natalia.desyatnikova.04@mail.ru>
2025-03-11 10:26:30 +0300

    Дописано сортировка по году или по фамилии

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,7 +2,6 @@ import ui.LibraryApp;
 
 public class Main {
     public static void main(String[] args) {
-        // Запуск приложения
         LibraryApp app = new LibraryApp();
         app.setVisible(true);
     }
